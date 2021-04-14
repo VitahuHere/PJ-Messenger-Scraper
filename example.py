38c2d9a60d3fec3ac28f3a49888541bff53dfb8e
Vitahu69 <79806896+Vitahu69@users.noreply.github.com>
2021-04-14 22:45:40 +0200

    Update and rename test.py to example.py

--- example.py
+++ example.py
@@ -1,13 +1,10 @@
-import Librus
 import messenger
 import time
 import os
 
 
-librus = Librus.Librus()
 chat = messenger.Chat()
-librus.log_in_librus('Login', 'Password')
 chat.send_picture('conversation thread', 'path to file', 'mail', 'password')
 chat.send_message('conversation thread', 'message', 'mail', 'password')
 time.sleep(4)
-librus.exit()
+chat.exit()
