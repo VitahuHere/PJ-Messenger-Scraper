c96f12ab2ad1edcf60cc6d14c28a1a57308ce5e6
Vitah <vumirek@gmail.com>
2023-03-28 02:28:56 +0200

    update main

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ password = base64.b64encode(config("FB_PASS").encode("utf-8")).decode("utf-8")
 
 messenger = (
     Facebook(email, password)
-    .send_message("Hello There", 100004933353411)
-    .send_message("This is automated message", 100004933353411)
+    .send_message("Hello There", 10000)
+    .send_message("This is automated message", 10000)
     .send_image(
-        r"C:\Users\vumir\PycharmProjects\MessengerChat\Zrzut ekranu 2023-03-01 211138.png",
-        100004933353411,
+        r"path_to_image",
+        10000,
     )
 )
